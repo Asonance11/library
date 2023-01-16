38efa8c36bca8dbe7fbee8537080a058e0fe6413
Oluwadabira <adigunsamuelaso@gmail.com>
2023-01-16 15:10:07 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,10 +5,9 @@ const author = document.getElementById('author');
 const pages = document.getElementById('pages');
 const readBook = document.getElementById('read');
 const newBook = document.getElementById('new-book');
-
+const bookSection = document.getElementById('library');
 let myLibrary = []; //array for storing books
 let bookAddition;
-const form = document.getElementById('book-form');
 
 addBook.addEventListener('click', () => {
 	formContainer.style.display = 'block';
@@ -16,54 +15,36 @@ addBook.addEventListener('click', () => {
 /*newBook.addEventListener('click', () => {
 	addBookToLibrary();
 });*/
-form.addEventListener('submit', function (e) {
-	addBookToLibrary();
-	createBook();
-	e.preventDefault();
-	form.reset();
-});
 
 //constructor
 function Book(title, author, pages, readBook) {
-	this.title = form.title.value;
-	this.author = form.author.value;
-	this.pages = form.pages.value;
-	this.readBook = form.readBook.checked;
+	this.title = title;
+	this.author = author;
+	this.pages = pages;
+	this.readBook = readBook;
 }
 
 //fuction to add a new book to the array
 function addBookToLibrary() {
 	formContainer.style.display = 'none';
 	e.preventDefault();
-	bookAddition = new Book(title, author, pages, readBook);
+	bookAddition = new Book(
+		title.value,
+		author.value,
+		pages.value,
+		readBook.checked
+	);
 	myLibrary.push(bookAddition);
-	/*displayBook();
-	form.reset();*/
+	createBook();
 }
-
-function displayBook() {
-	const bookSection = document.getElementById('library');
-
-	const books = document.querySelectorAll('.book');
-	books.forEach((book) => bookSection.removeChild(book));
-
-	for (let i = 0; i < myLibrary.length; i++) {
-		createBook(myLibrary[i]);
-	}
-}
-
 //function to create the book div with dom elements
 function createBook(item) {
-	const library = document.querySelector('#library');
 	const bookDiv = document.createElement('div');
 	const titleDiv = document.createElement('div');
 	const authorDiv = document.createElement('div');
 	const pagesDiv = document.createElement('div');
 	const readDiv = document.createElement('button');
 	const removeDiv = document.createElement('button');
-
-	bookDiv.classList.add('book');
-	bookDiv.setAttribute('id', myLibrary.indexOf(item));
 
 	titleDiv.textContent = item.title;
 	titleDiv.classList.add('title');
@@ -79,29 +60,9 @@ function createBook(item) {
 
 	bookDiv.appendChild(readDiv);
 	readDiv.classList.add('read');
-	if (item.read === false) {
-		readDiv.textContent = 'Not Read';
-		readDiv.style.backgroundColor = '#e04f63';
-	} else {
-		readDiv.textContent = 'Read';
-		readDiv.style.backgroundColor = '#63da63';
-	}
-
-	removeDiv.textContent = 'Remove';
-	removeDiv.setAttribute('id', 'removeDiv');
-
-	removeDiv.classList.add('remove');
 	bookDiv.appendChild(removeDiv);
-	removeDiv.addEventListener('click', () => {
-		myLibrary.splice(myLibrary.indexOf(item), 1);
-		displayBook();
-	});
+	removeDiv.classList.add('remove');
 
-	//add toggle ability to each book 'read' button on click
-	readDiv.addEventListener('click', () => {
-		item.read = !item.read;
-		displayBook();
-	});
-
-	library.appendChild(bookDiv);
+	bookDiv.classList.add('book');
+	bookSection.appendChild(bookDiv);
 }
